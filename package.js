cabc995ced5883a0c70daf9023a9389ccd22dd1c
yyyet-uu <yafet.tech0990@gmail.com>
2026-08-18 16:09:28 +0300

    Update package.js

--- package.js
+++ package.js
@@ -3,7 +3,11 @@
   "version": "1.0.0",
   "private": true,
   "type": "module",
+  "engines": {
+    "node": "22.x"
+  },
   "dependencies": {
-    "firebase-admin": "^13.4.0"
+    "ethers": "^6.15.0",
+    "firebase-admin": "^13.5.0"
   }
 }
